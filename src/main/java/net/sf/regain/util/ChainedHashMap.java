dc56d2498ac5089fe967f0f43f53c63092e7e8d8
Antling Wu <wu.antlingqs@gmail.com>
2022-06-10 21:53:56 +0800

    Change regain to maven project

--- src/main/java/net/sf/regain/util/ChainedHashMap.java
+++ src/main/java/net/sf/regain/util/ChainedHashMap.java
@@ -20,8 +20,6 @@
  */
 package net.sf.regain.util;
 
-
-
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -35,291 +33,283 @@ import java.util.Set;
 /**
  * Hashtable that can contain several entries per key.
  * (Helper Class)
- *
+ * <p>
  * Contract:
  * <li>It is possible to put several identical key-value pairs (i.e. where key and value is equal)
  * <li>entrySet is not supported. Instead, it can be iterated over all entries.
  *
- * @param <K>	Key
- * @param <V>	Value
+ * @param <K> Key
+ * @param <V> Value
  */
 public class ChainedHashMap<K, V> implements Map<K, V>, Iterable<Map.Entry<K, V>> {
 
-	private static final int DEFAULT_HASHTABLE_SIZE = 20;
+    private static final int DEFAULT_HASHTABLE_SIZE = 20;
 
-	private static final int DEFAULT_LIST_SIZE = 10;
-	private int listSize;
+    private static final int DEFAULT_LIST_SIZE = 10;
+    private int listSize;
 
-	HashMap<K, List<V>> hashtable;
-	int size;
+    HashMap<K, List<V>> hashtable;
+    int size;
 
-	public ChainedHashMap()
-	{
-		this(DEFAULT_HASHTABLE_SIZE);
-	}
+    public ChainedHashMap() {
+        this(DEFAULT_HASHTABLE_SIZE);
+    }
 
-	public ChainedHashMap(int hashtableSize) {
-		this(hashtableSize, DEFAULT_LIST_SIZE);
-	}
-	public ChainedHashMap(int hashtableSize, int chainSize) {
-		hashtable = new HashMap<K, List<V>>(hashtableSize);
-		listSize = chainSize;
+    public ChainedHashMap(int hashtableSize) {
+        this(hashtableSize, DEFAULT_LIST_SIZE);
+    }
 
-		size = 0;
-	}
+    public ChainedHashMap(int hashtableSize, int chainSize) {
+        hashtable = new HashMap<K, List<V>>(hashtableSize);
+        listSize = chainSize;
 
-	public ChainedHashMap(Map<? extends K, ? extends V> map)
-	{
-		this();
+        size = 0;
+    }
 
-		if (map instanceof ChainedHashMap)
-		{
-			// Copy-constructor
-			ChainedHashMap<? extends K, ? extends V> hashtable = (ChainedHashMap<? extends K, ? extends V>) map;
-			for (K key : hashtable.keySet())
-			{
-				for (V value: hashtable.getList(key))
-				{
-					put(key, value);
-				}
-			}
-		}
-		else
-			putAll(map);
-	}
+    public ChainedHashMap(Map<? extends K, ? extends V> map) {
+        this();
 
-	@Override
-	public int size() {
-		return size;
-	}
+        if (map instanceof ChainedHashMap) {
+            // Copy-constructor
+            ChainedHashMap<? extends K, ? extends V> hashtable = (ChainedHashMap<? extends K, ? extends V>) map;
+            for (K key : hashtable.keySet()) {
+                for (V value : hashtable.getList(key)) {
+                    put(key, value);
+                }
+            }
+        } else
+            putAll(map);
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    @Override
+    public int size() {
+        return size;
+    }
 
-	@Override
-	public boolean containsKey(Object key) {
-		return hashtable.containsKey(key);
-	}
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	@Override
-	public boolean containsValue(Object value) {
-		if (isEmpty())
-			return false;
+    @Override
+    public boolean containsKey(Object key) {
+        return hashtable.containsKey(key);
+    }
 
-		Collection<List<V>> elements = hashtable.values();
+    @Override
+    public boolean containsValue(Object value) {
+        if (isEmpty())
+            return false;
 
-		for (List<V> list: elements)
-		{
-			if (list.contains(value))
-				return true;
-		}
-		return false;
-	}
+        Collection<List<V>> elements = hashtable.values();
 
-	@Override
-	/**
-	 * Get first of the linked objects by this key.
-	 */
-	public V get(Object key) {
-		List<V> list = hashtable.get(key);
-		if (list == null)
-			return null;
-		else
-			return list.get(0);
-	}
+        for (List<V> list : elements) {
+            if (list.contains(value))
+                return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Get all objects linked by this key
-	 * as an Iterable usable an foreach loop.
-	 *
-	 * @param key
-	 * @return	Iterable
-	 * @throws NullPointerException (if key null)
-	 */
-	public Iterable<V> getIterable(Object key) {
-		final List<V> list = hashtable.get(key);
+    @Override
+    /**
+     * Get first of the linked objects by this key.
+     */
+    public V get(Object key) {
+        List<V> list = hashtable.get(key);
+        if (list == null)
+            return null;
+        else
+            return list.get(0);
+    }
 
-		if (list == null)
-		{
-			// Empty Iterator
-			return new Iterable<V>() {
-				public Iterator<V> iterator() {
-					return new Iterator<V>() {
-						public boolean hasNext() { return false; }
-						public V next() { throw new NoSuchElementException("Empty"); }
-						public void remove() { }
-					};
-				}
-			};
-		}
-		else
-		{
-			// Iterator of list
-			return new Iterable<V>() {
-				public Iterator<V> iterator() {
-					return list.iterator();
-				}
-			};
-		}
-	}
+    /**
+     * Get all objects linked by this key
+     * as an Iterable usable an foreach loop.
+     *
+     * @param key
+     * @return Iterable
+     * @throws NullPointerException (if key null)
+     */
+    public Iterable<V> getIterable(Object key) {
+        final List<V> list = hashtable.get(key);
 
-	public List<V> getList(Object key)
-	{
-	  List<V> list = hashtable.get(key);
-		if (list == null)
-		  list = new ArrayList<V>();
-		return list;
-	}
+        if (list == null) {
+            // Empty Iterator
+            return new Iterable<V>() {
+                public Iterator<V> iterator() {
+                    return new Iterator<V>() {
+                        public boolean hasNext() {
+                            return false;
+                        }
 
-	/**
-	 * Iterate over all elements in the table.
-	 * Note that this currently copies them into a collection,
-	 * so concurrent modification will not be taken into account
-	 * (there will be no ConcurrentModificationException, either).
-	 */
-	@Override
-	public Iterator<Map.Entry<K, V>> iterator() {
-		if (size == 0)
-		{
-			return new Iterator<Map.Entry<K, V>>() {
-				public boolean hasNext() { return false; }
-				public Map.Entry<K, V> next() { throw new NoSuchElementException("Empty"); }
-				public void remove() { }
-			};
-		}
-		else
-		{
-			Collection<Map.Entry<K, V>> entries = new ArrayList<Map.Entry<K, V>>();
-			for (K key : hashtable.keySet())
-			{
-				List<V> values = hashtable.get(key);
-				for (V value : values)
-					entries.add(new AbstractMap.SimpleEntry<K,V>(key, value));
-			}
-			return entries.iterator();
-		}
-	}
+                        public V next() {
+                            throw new NoSuchElementException("Empty");
+                        }
 
-	@Override
-	/**
-	 * Add this Value at the end of this key.
-	 *
-	 * @return As the value is never replaced, this will always return null.
-	 */
-	public V put(K key, V value) {
-		boolean success;
+                        public void remove() {
+                        }
+                    };
+                }
+            };
+        } else {
+            // Iterator of list
+            return new Iterable<V>() {
+                public Iterator<V> iterator() {
+                    return list.iterator();
+                }
+            };
+        }
+    }
 
-		List<V> list = hashtable.get(key);
-		if (list == null)
-		{
-			list = new ArrayList<V>(listSize);
-			success = list.add(value);
-			hashtable.put(key, list);
-		}
-		else
-		{
-			success = list.add(value);
-		}
+    public List<V> getList(Object key) {
+        List<V> list = hashtable.get(key);
+        if (list == null)
+            list = new ArrayList<V>();
+        return list;
+    }
 
-		if (success)
-			size++;
+    /**
+     * Iterate over all elements in the table.
+     * Note that this currently copies them into a collection,
+     * so concurrent modification will not be taken into account
+     * (there will be no ConcurrentModificationException, either).
+     */
+    @Override
+    public Iterator<Map.Entry<K, V>> iterator() {
+        if (size == 0) {
+            return new Iterator<Map.Entry<K, V>>() {
+                public boolean hasNext() {
+                    return false;
+                }
 
-		return null;
-	}
+                public Map.Entry<K, V> next() {
+                    throw new NoSuchElementException("Empty");
+                }
 
-	@Override
-	/**
-	 * Remove all objects linked to this key.
-	 *
-	 * @param key	Key
-	 * @return First of linked objects (or null).
-	 */
-	public V remove(Object key) {
-		List<V> list = hashtable.remove(key);
-		if (list == null)
-			return null;
-		else
-		{
-			V element = list.get(0);
-			size -= list.size();
-			return element;
-		}
-	}
+                public void remove() {
+                }
+            };
+        } else {
+            Collection<Map.Entry<K, V>> entries = new ArrayList<Map.Entry<K, V>>();
+            for (K key : hashtable.keySet()) {
+                List<V> values = hashtable.get(key);
+                for (V value : values)
+                    entries.add(new AbstractMap.SimpleEntry<K, V>(key, value));
+            }
+            return entries.iterator();
+        }
+    }
 
-	public boolean remove(K key, V value)
-	{
-		List<V> list = hashtable.get(key);
+    @Override
+    /**
+     * Add this Value at the end of this key.
+     *
+     * @return As the value is never replaced, this will always return null.
+     */
+    public V put(K key, V value) {
+        boolean success;
 
-		if (list == null)
-			return false;
+        List<V> list = hashtable.get(key);
+        if (list == null) {
+            list = new ArrayList<V>(listSize);
+            success = list.add(value);
+            hashtable.put(key, list);
+        } else {
+            success = list.add(value);
+        }
 
-		boolean removed = list.remove(value);
-		if (removed)
-		{
-			if (list.isEmpty())
-				hashtable.remove(key);
-			size--;
-		}
-		return removed;
-	}
+        if (success)
+            size++;
 
-	@Override
-	public void putAll(Map<? extends K, ? extends V> map) {
-		for (Entry<? extends K, ? extends V> entry : map.entrySet()) {
-			put(entry.getKey(), entry.getValue());
-		}
-	}
+        return null;
+    }
 
-	@Override
-	public void clear() {
-		hashtable.clear();
-		size = 0;
-	}
+    @Override
+    /**
+     * Remove all objects linked to this key.
+     *
+     * @param key    Key
+     * @return First of linked objects (or null).
+     */
+    public V remove(Object key) {
+        List<V> list = hashtable.remove(key);
+        if (list == null)
+            return null;
+        else {
+            V element = list.get(0);
+            size -= list.size();
+            return element;
+        }
+    }
 
-	@Override
-	public Set<K> keySet() {
-		return hashtable.keySet();
-	}
+    public boolean remove(Object key, Object value) {
+        List<V> list = hashtable.get(key);
 
-	@Override
-	// TODO "The set is backed by the map, so changes to the map are reflected in the set, and vice-versa."
-	public Collection<V> values() {
-		List<V> newList = new ArrayList<V>();
+        if (list == null)
+            return false;
 
-		if (isEmpty())
-			return newList;
+        boolean removed = list.remove(value);
+        if (removed) {
+            if (list.isEmpty())
+                hashtable.remove(key);
+            size--;
+        }
+        return removed;
+    }
 
-		Collection<List<V>> values = hashtable.values();
+    @Override
+    public void putAll(Map<? extends K, ? extends V> map) {
+        for (Entry<? extends K, ? extends V> entry : map.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
 
-		for(List<V> list : values )
-			newList.addAll(list);
+    @Override
+    public void clear() {
+        hashtable.clear();
+        size = 0;
+    }
 
-		return newList;
-	}
+    @Override
+    public Set<K> keySet() {
+        return hashtable.keySet();
+    }
 
-	@Override
-	public Set<Map.Entry<K, V>> entrySet() {
-		throw new UnsupportedOperationException("entrySet is not implemented, as identical entries are allowed (conflict with Set contract). Instead, use .iterator() to iterate through all entries.");
-	}
+    @Override
+    // TODO "The set is backed by the map, so changes to the map are reflected in the set, and vice-versa."
+    public Collection<V> values() {
+        List<V> newList = new ArrayList<V>();
 
-	public String toString()
-	{
-		StringBuffer str = new StringBuffer(200);
+        if (isEmpty())
+            return newList;
 
-		for (K key : hashtable.keySet())
-		{
-			str.append(key).append(":\n");
+        Collection<List<V>> values = hashtable.values();
 
-			List<V> values = hashtable.get(key);
-			for (V value : values)
-				str.append("\t").append(value).append("\n");
-			str.append("\n");
-		}
+        for (List<V> list : values)
+            newList.addAll(list);
 
-		return str.toString();
-	}
+        return newList;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        throw new UnsupportedOperationException("entrySet is not implemented, as identical entries are allowed (conflict with Set contract). Instead, use .iterator() to iterate through all entries.");
+    }
+
+    public String toString() {
+        StringBuffer str = new StringBuffer(200);
+
+        for (K key : hashtable.keySet()) {
+            str.append(key).append(":\n");
+
+            List<V> values = hashtable.get(key);
+            for (V value : values)
+                str.append("\t").append(value).append("\n");
+            str.append("\n");
+        }
+
+        return str.toString();
+    }
 
 
 }
